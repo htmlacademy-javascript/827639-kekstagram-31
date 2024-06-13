9fe0b31a20b219479ec5b0ddb1de060eea801d3e
Sonya Yuyukina <yuyukinasofia@gmail.com>
2024-06-13 18:43:27 +0700

    Добавляет задание с проверкой встречи

--- js/functions.js
+++ js/functions.js
@@ -42,3 +42,28 @@ function returnNumbers(string) {
 // console.log(returnNumbers('1 кефир, 0.5 батона'));
 // console.log(returnNumbers('агент 007'));
 // console.log(returnNumbers('а я томат'));
+
+//Задание модуль 5
+
+function turnIntoMinutes(time) {
+  const timeNumbers = time.split(':');
+  const timeInMinutes = +timeNumbers[0] * 60 + +timeNumbers[1];
+  return timeInMinutes;
+}
+
+console.log(turnIntoMinutes('08:05'));
+
+function checkMeeting(start, end, meetingStart, duration) {
+  const startInMinutes = turnIntoMinutes(start);
+  const endInMinutes = turnIntoMinutes(end);
+  const meetingStartInMinutes = turnIntoMinutes(meetingStart);
+  const meetingEndInMinutes = meetingStartInMinutes + duration;
+  const result = meetingStartInMinutes >= startInMinutes && meetingStartInMinutes <= endInMinutes && meetingEndInMinutes >= startInMinutes && meetingEndInMinutes <= endInMinutes;
+  return result;
+}
+
+// console.log(checkMeeting('08:00', '17:30', '14:00', 90));
+// console.log(checkMeeting('8:0', '10:0', '8:0', 120));
+// console.log(checkMeeting('08:00', '14:30', '14:00', 90));
+// console.log(checkMeeting('14:00', '17:30', '08:0', 90));
+// console.log(checkMeeting('8:00', '17:30', '08:00', 900));
